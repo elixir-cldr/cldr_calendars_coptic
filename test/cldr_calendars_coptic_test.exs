1d9c22e5086b968437b5ba07e27de313d89adc63
Kip Cole <kipcole9@gmail.com>
2021-10-03 10:36:35 +0800

    Update test description

--- test/cldr_calendars_coptic_test.exs
+++ test/cldr_calendars_coptic_test.exs
@@ -22,7 +22,7 @@ defmodule Cldr.Calendar.CopticTest do
     assert Cldr.Calendar.Coptic.months_in_year(2000) == 13
   end
 
-  test "~D sigil" do
+  test "~U sigil" do
     assert ~U[1736-13-01T00:00:00.0Z Cldr.Calendar.Coptic]
     assert ~D[1736-13-01 Cldr.Calendar.Coptic]
     assert ~D[1736-13-05 Cldr.Calendar.Coptic]
